1b2915d0033ae6b7764094a1c6b21c04786473a2
Liliane <159821845+lilianesoares1@users.noreply.github.com>
2025-07-11 18:19:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function () {
   let bot = "Lilly";
 
   const apresentacao = "Olá, eu sou a " + bot + ".";
-  const saudacao = "Estou empolgada para falar com você.";
+  const saudacao = "Estou empolgada para falar com vocês.";
   const introducao = "Hoje eu vim dizer a vocês a minha comida favorita.";
   const comidafavorita = "Estrogonofe de Frango";
   const comida = "A minha comida favorita é " + comidafavorita + ".";
